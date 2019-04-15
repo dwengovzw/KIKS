01d598adac7f208cffedee7e922ada0cc70d0319
Alexander Meheus <meheusalexander@gmail.com>
2019-04-15 10:37:11 +0200

    herwerken notebooks met feedback

--- .scripts/diep_neuraal_netwerk.py
+++ .scripts/diep_neuraal_netwerk.py
@@ -3,9 +3,6 @@ from pymongo import MongoClient
 from PIL import Image
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from matplotlib.ticker import MaxNLocator
-from multiprocessing import Process, Queue
-from ipyupload import FileUpload
-from keras.models import load_model
 import keras.backend as K
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,10 +23,8 @@ layout = widgets.Layout(width='30%')
 conv_layers_widget = widgets.HBox([widgets.Label('Aantal convolutionele lagen:', layout=layout), widgets.IntSlider(value=1, min=1, max=3)])
 ff_layers_widget = widgets.HBox([widgets.Label('Aantal feedforward lagen:', layout=layout), widgets.IntSlider(value=1, min=1, max=2)])
 ff_input_widget = widgets.HBox([widgets.Label('Aantal neuronen in eerste feedforward laag:', layout=layout), widgets.Dropdown(options=[16, 64, 256, 1024])])
-learning_rate_widget = widgets.HBox([widgets.Label('learning rate:', layout=layout), widgets.SelectionSlider(options=[0.01, 0.001, 0.0001])])
-epochs_widget = widgets.HBox([widgets.Label('Aantal epochs:', layout=layout), widgets.IntSlider(value=10, min=1, max=20)])
-
-upload_widget = FileUpload(accept='image/*')
+learning_rate_widget = widgets.HBox([widgets.Label('learning rate:', layout=layout), widgets.SelectionSlider(value=0.01, options=[0.1, 0.01, 0.001, 0.0001])])
+epochs_widget = widgets.HBox([widgets.Label('Aantal epochs:', layout=layout), widgets.IntSlider(value=20, min=1, max=20)])
 
 client = MongoClient()
 db = client.models
@@ -43,7 +38,7 @@ model_attr = {
     'ff_input': 16,
     'optimizer': 'sgd',
     'learning_rate': 0.01,
-    'epoch': 10
+    'epoch': 20
 }
 
 model = None
@@ -64,19 +59,22 @@ def get_model():
     model = models_collection.find_one({'name': name, 'epoch': (model_attr['epoch'] - 1)})
 
 
+get_model()
 conv_layers_widget.children[1].observe(lambda change: update_model('conv_layers', change['new']), names='value')
 ff_layers_widget.children[1].observe(lambda change: update_model('ff_layers', change['new']), names='value')
 ff_input_widget.children[1].observe(lambda change: update_model('ff_input', change['new']), names='value')
 learning_rate_widget.children[1].observe(lambda change: update_model('learning_rate', change['new']), names='value')
-epochs_widget.children[1].observe(lambda change: update_model('epoch', change['new']), names='value')
+#epochs_widget.children[1].observe(lambda change: update_model('epoch', change['new']), names='value')
 
 
-def kies_netwerk():
+def kies_netwerk_parameters():
     display(conv_layers_widget)
     display(ff_layers_widget)
     display(ff_input_widget)
+
+
+def kies_training_parameters():
     display(learning_rate_widget)
-    display(epochs_widget)
 
 
 def toon_netwerk():
@@ -84,33 +82,19 @@ def toon_netwerk():
     display(im)
 
 
-def toon_loss():
-    print('Training loss:\t\t' + str(model['loss']))
-    print('Validation loss:\t' + str(model['val_loss']))
-    print('Test loss:\t\t' + str(model['test_loss']))
-
-
-def toon_accuracy():
-    print('Training accuracy:\t' + str(model['acc']))
-    print('Validation accuracy:\t' + str(model['val_acc']))
-    print('Test accuracy:\t\t' + str(model['test_acc']))
-
-
-def toon_afbeeldingen():
-    fig, big_axes = plt.subplots(figsize=(7, 5), nrows=2, ncols=1, sharex=True, sharey=True)
+def toon_voorspellingen():
+    fig, big_axes = plt.subplots(figsize=(14, 5), nrows=2, ncols=1, sharex=True, sharey=True)
     fig.subplots_adjust(hspace=0.25)
 
     big_axes[0].set_title("Afbeeldingen met stoma")
     big_axes[1].set_title("Afbeeldingen zonder stoma")
-    big_axes[0].set_xticks([])
-    big_axes[1].set_yticks([])
-    big_axes[0]._frameon = False
-    big_axes[1]._frameon = False
+    big_axes[0].set_axis_off()
+    big_axes[1].set_axis_off()
 
     images = model['stoma_patches'] + model['no_stoma_patches']
     for i, image in enumerate(images):
         im = Image.open(os.path.join(image_dir, image['name']))
-        ax = fig.add_subplot(2, 3, i + 1)
+        ax = fig.add_subplot(2, 6, i + 1)
         ax.imshow(im)
 
         xlabel = "Pred: {:f}".format(float(image['prediction']))
@@ -122,39 +106,66 @@ def toon_afbeeldingen():
     plt.show()
 
 
-def toon_loss_grafiek():
-    loss = []
-    val_loss = []
+def toon_slechte_voorspellingen():
+    fig, big_axes = plt.subplots(figsize=(14, 5), nrows=2, ncols=1, sharex=True, sharey=True)
+    fig.subplots_adjust(hspace=0.25)
 
-    for temp_model in models_collection.find({'name': model['name'], 'epoch': {"$lte": model['epoch']}}).sort('epoch', 1):
-        loss.append(temp_model['loss'])
-        val_loss.append(temp_model['val_loss'])
+    big_axes[0].set_title("Afbeeldingen met stoma (false negative)")
+    big_axes[1].set_title("Afbeeldingen zonder stoma (false positive)")
+    big_axes[0].set_axis_off()
+    big_axes[1].set_axis_off()
 
-    ax = plt.figure().gca()
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.plot(range(1, len(loss) + 1), loss, 'b', label='Training loss')
-    plt.plot(range(1, len(val_loss) + 1), val_loss, 'g', label='Validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
+    images = model['false_negatives'] + model['false_positives']
+    for i, image in enumerate(images):
+        im = Image.open(os.path.join(image_dir, image['name']))
+        ax = fig.add_subplot(2, 6, i + 1)
+        ax.imshow(im)
+
+        xlabel = "Pred: {:f}".format(float(image['prediction']))
+        ax.set_xlabel(xlabel)
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+
     plt.show()
 
 
-def toon_accuracy_grafiek():
+def toon_test_resultaten():
+    print('Test loss:\t' + str(model['test_loss']))
+    print('Test accuracy:\t' + str(model['test_acc']))
+
+
+def toon_grafiek():
+    loss = []
+    val_loss = []
     acc = []
     val_acc = []
-
     for temp_model in models_collection.find({'name': model['name'], 'epoch': {"$lte": model['epoch']}}).sort('epoch', 1):
+        loss.append(temp_model['loss'])
+        val_loss.append(temp_model['val_loss'])
         acc.append(temp_model['acc'])
         val_acc.append(temp_model['val_acc'])
 
-    ax = plt.figure().gca()
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.plot(range(1, len(acc) + 1), acc, 'b', label='Training accuracy')
-    plt.plot(range(1, len(val_acc) + 1), val_acc, 'g', label='Validation accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
+    fig, ax = plt.subplots(figsize=(14, 5), nrows=1, ncols=2)
+
+    title_1 = ('Training loss: \t\t' + str(model['loss']) + '\nValidation loss:\t' + str(model['val_loss'])).expandtabs()
+    ax[0].set_title(title_1, loc='left')
+    ax[0].xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax[0].plot(range(1, len(loss) + 1), loss, 'b', label='Training loss')
+    ax[0].plot(range(1, len(val_loss) + 1), val_loss, 'g', label='Validation loss')
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('Loss')
+    ax[0].legend()
+
+    title_2 = ('Training accuracy: \t' + str(model['acc']) + '\nValidation accuracy:\t' + str(model['val_acc'])).expandtabs()
+    ax[1].set_title(title_2, loc='left')
+    ax[1].xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax[1].plot(range(1, len(acc) + 1), acc, 'b', label='Training accuracy')
+    ax[1].plot(range(1, len(val_acc) + 1), val_acc, 'g', label='Validation accuracy')
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('Loss')
+    ax[1].legend()
+
     plt.show()
 
 
@@ -205,91 +216,6 @@ def laad_referentie_model():
         'epoch': 20
     }
     get_model()
-
-
-def geef_gekozen_afbeelding():
-    if not upload_widget.value:
-        print('Je hebt nog geen file gekozen.')
-        return None
-
-    first_key = next(iter(upload_widget.value))
-
-    if not upload_widget.value[first_key]['metadata']['type'].startswith('image'):
-        print('Kies een afbeelding.')
-        return None
-
-    image = Image.open(io.BytesIO(upload_widget.value[first_key]['content']))
-    return image
-
-
-def vind_stomata_subproces(image, q):
-    model_file = load_model(os.path.join(model_dir, model['name'] + '_model.h5'))
-
-    shift = 10
-    offset = 60
-    bandwidth = offset
-
-    im_r = np.array(image)
-
-    stomata_punten = {}
-    for thr in range(5, 100, 5):
-        stomata_punten[str(thr)] = []
-
-    no_x_shifts = (np.shape(im_r)[0] - 2 * offset) // shift
-    no_y_shifts = (np.shape(im_r)[1] - 2 * offset) // shift
-
-    for x in np.arange(no_x_shifts + 1):
-        for y in np.arange(no_y_shifts + 1):
-            x_c = x * shift + offset
-            y_c = y * shift + offset
-
-            im_r_crop = im_r[x_c - offset:x_c + offset, y_c - offset:y_c + offset, :]
-            im_r_crop = im_r_crop.astype('float32')
-            im_r_crop /= 255
-
-            y_model = model_file.predict(np.expand_dims(im_r_crop, axis=0))
-
-            for thr in range(5, 100, 5):
-                if y_model[0][0] > thr / 100.:
-                    stomata_punten[str(thr)].append([x_c, y_c])
-
-    for thr in range(5, 100, 5):
-        if stomata_punten[str(thr)]:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-
-                ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-                ms.fit(stomata_punten[str(thr)])
-                stomata_punten[str(thr)] = [[x[1], x[0]] for x in ms.cluster_centers_]  # Because cluster_centers is inverted
-
-    q.put(stomata_punten)
-
-
-def vind_stomata_eigen_afbeelding():
-    image = geef_gekozen_afbeelding()
-    if image is None:
-        return
-
-    # We voeren dit uit in een appart proces omdat de gpu memory dan wordt vrijgegeven
-    q = Queue()
-    p = Process(target=vind_stomata_subproces, args=(image, q))
-    p.start()
-    p.join()
-    stomata_punten = q.get()
-
-    fig, ax = plt.subplots()
-    ax.imshow(image)
-    points_im, = ax.plot([], [], '+c', alpha=0.6, markeredgewidth=3, markersize=12)
-    plt.close()
-
-    def change_threshold(thr=50):
-        x_points = [x[0] for x in stomata_punten[str(thr)]]
-        y_points = [x[1] for x in stomata_punten[str(thr)]]
-        points_im.set_xdata(x_points)
-        points_im.set_ydata(y_points)
-        display(fig)
-
-    widgets.interact(change_threshold, thr=widgets.IntSlider(value=50, min=5, max=95, step=5, continuous_update=False))
 
 
 def misleid_netwerk():
---
+++ .scripts/vind_stomata.py
@@ -0,0 +1,135 @@
+from PIL import Image
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from keras.models import load_model
+from ipyupload import FileUpload
+from multiprocessing import Process, Queue
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import ipywidgets as widgets
+import numpy as np
+import warnings
+import io
+import os
+
+reference_model = 'detecting_stomata_model_VGG19FT.h5'
+model_dir = '../.data/IntroductieDeepLearning'
+
+upload_widget = FileUpload(accept='image/*')
+
+image_crop = None
+
+
+def geef_gekozen_afbeelding():
+    if not upload_widget.value:
+        print('Je hebt nog geen file gekozen.')
+        return None
+
+    first_key = next(iter(upload_widget.value))
+
+    if not upload_widget.value[first_key]['metadata']['type'].startswith('image'):
+        print('Kies een afbeelding.')
+        return None
+
+    image = Image.open(io.BytesIO(upload_widget.value[first_key]['content']))
+    return image
+
+
+def toon_eigen_afbeelding():
+    image = geef_gekozen_afbeelding()
+    if image is None:
+        return
+
+    im_r = np.array(image)
+
+    height, width, depth = im_r.shape
+    rect_width = width
+    rect_height = height
+    if rect_width > 800:
+        rect_width = 800
+    if rect_height > 800:
+        rect_height = 800
+
+    fig = plt.figure()
+    plt.imshow(im_r)
+    rect = patches.Rectangle((0, 0), rect_width, rect_height, linewidth=-1, edgecolor='r', facecolor='none')
+    ax = plt.gca()
+    ax.add_patch(rect)
+    plt.close()
+
+    def choose_regio(x, y):
+        global image_crop
+        image_crop = im_r[y: y + rect_height, x: x + rect_width, :]
+        rect.set_xy((x, y))
+        display(fig)
+
+    max_x = max(width - 800, 0)
+    max_y = max(height - 800, 0)
+    widgets.interact(choose_regio, x=widgets.IntSlider(min=0, max=max_x), y=widgets.IntSlider(min=0, max=max_y))
+
+
+def vind_stomata_subproces(im_r, q):
+    model_file = load_model(os.path.join(model_dir, reference_model))
+
+    shift = 10
+    offset = 60
+    bandwidth = offset
+
+    stomata_punten = {}
+    for thr in range(5, 100, 5):
+        stomata_punten[str(thr)] = []
+
+    no_x_shifts = (np.shape(im_r)[0] - 2 * offset) // shift
+    no_y_shifts = (np.shape(im_r)[1] - 2 * offset) // shift
+
+    for x in np.arange(no_x_shifts + 1):
+        for y in np.arange(no_y_shifts + 1):
+            x_c = x * shift + offset
+            y_c = y * shift + offset
+
+            im_r_crop = im_r[x_c - offset:x_c + offset, y_c - offset:y_c + offset, :]
+            im_r_crop = im_r_crop.astype('float32')
+            im_r_crop /= 255
+
+            y_model = model_file.predict(np.expand_dims(im_r_crop, axis=0))
+
+            for thr in range(5, 100, 5):
+                if y_model[0][1] > thr / 100.:
+                    stomata_punten[str(thr)].append([x_c, y_c])
+
+    for thr in range(5, 100, 5):
+        if stomata_punten[str(thr)]:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+
+                ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+                ms.fit(stomata_punten[str(thr)])
+                stomata_punten[str(thr)] = [[x[1], x[0]] for x in ms.cluster_centers_]  # Because cluster_centers is inverted
+
+    q.put(stomata_punten)
+
+
+def vind_stomata_eigen_afbeelding():
+    global image_crop
+    if image_crop is None:
+        return
+
+    # We voeren dit uit in een appart proces omdat de gpu memory dan wordt vrijgegeven
+    q = Queue()
+    p = Process(target=vind_stomata_subproces, args=(image_crop, q))
+    p.start()
+    p.join()
+    stomata_punten = q.get()
+
+    fig, ax = plt.subplots()
+    ax.imshow(image_crop)
+    points_im, = ax.plot([], [], '+c', alpha=0.6, markeredgewidth=3, markersize=12)
+    plt.close()
+
+    def change_threshold(thr=50):
+        x_points = [x[0] for x in stomata_punten[str(thr)]]
+        y_points = [x[1] for x in stomata_punten[str(thr)]]
+        points_im.set_xdata(x_points)
+        points_im.set_ydata(y_points)
+        display(fig)
+
+    widgets.interact(change_threshold, thr=widgets.IntSlider(value=50, min=5, max=95, step=5, continuous_update=False))
